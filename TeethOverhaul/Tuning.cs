f895681788d8c5e53c679bc3ff1a37aeb6affe51
CreatorAndrew <andrew.noah.mitchell@gmail.com>
2026-06-23 09:17:05 -0500

    Simplify tuning

--- TeethOverhaul/Tuning.cs
+++ TeethOverhaul/Tuning.cs
@@ -1,19 +1,8 @@
-﻿using Sims3.SimIFace;
-
-namespace Sims3.Gameplay.SimsVerse
+﻿namespace Sims3.Gameplay.SimsVerse
 {
     public class TeethOverhaul
     {
-        [Tunable]
-        public static bool kAutoRandomizeTeethOnSimAgeTransition;
-
-        [Tunable]
-        public static bool kAutoRandomizeTeethOnSimInstantiated;
-
-        [Tunable]
-        public static bool kInteractionsAreCheats;
-
-        [Tunable]
-        public static bool kShowInteractions;
+        [Sims3.SimIFace.Tunable]
+        public static bool kAutoRandomizeTeethOnSimAgeTransition, kAutoRandomizeTeethOnSimInstantiated, kInteractionsAreCheats, kShowInteractions;
     }
 }
